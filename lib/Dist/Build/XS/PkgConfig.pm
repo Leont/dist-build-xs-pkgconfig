f5a414a8d205a6c77d20e7b7c0c193e528cafd9d
Leon Timmermans <fawaka@gmail.com>
2024-09-13 18:28:18 +0200

    Initial commit

---
+++ lib/Dist/Build/XS/PkgConfig.pm
@@ -0,0 +1,35 @@
+package Dist::Build::XS::PkgConfig;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.000';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Build::XS::PkgConfig - INSERT YOUR ABSTRACT HERE
+
+=head1 DESCRIPTION
+
+Write a full description of the module and its features here.
+
+=head1 AUTHOR
+
+Leon Timmermans <fawaka@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2024 by Leon Timmermans.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
